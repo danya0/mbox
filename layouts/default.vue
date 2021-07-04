762ec86575c281678be31df3459c303805c738b6
danya0 <samarkin.danil@yandex.ru>
2021-07-04 11:22:43 +0300

    initial commit

---
+++ layouts/default.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <header></header>
+    <Nuxt/>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+header {
+  background: $bg;
+  width: 100%;
+  height: 100px;
+}
+</style>
